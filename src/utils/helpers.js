df5575a64dcc3d8ffa35e43f4cc5a661d4dc3f3d
Omar <omarmuhammed1998.om@gmail.com>
2021-06-06 01:52:25 +0200

    created helpers module

---
+++ src/utils/helpers.js
@@ -0,0 +1,22 @@
+export function generateUID() {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+}
+
+export function formatQuestion({ optionOneText, optionTwoText, author }) {
+  return {
+    id: generateUID(),
+    timestamp: Date.now(),
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    },
+  };
+}
