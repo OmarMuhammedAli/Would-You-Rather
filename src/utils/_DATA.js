956d3f1485ad9bb389af7da2976cd54cedd57f99
Omar <omarmuhammed1998.om@gmail.com>
2021-06-06 01:56:13 +0200

    modularized _DATA module

---
+++ src/utils/_DATA.js
@@ -0,0 +1,77 @@
+import {formatQuestion} from './helpers'
+import {questionsObject} from './db/questions.json'
+import {usersObject} from './db/users.json'
+
+let users = usersObject
+
+let questions = questionsObject
+
+
+
+export function _getUsers () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...users}), 1000)
+  })
+}
+
+export function _getQuestions () {
+  return new Promise((res, rej) => {
+    setTimeout(() => res({...questions}), 1000)
+  })
+}
+
+
+
+export function _saveQuestion (question) {
+  return new Promise((res, rej) => {
+    const authedUser = question.author;
+    const formattedQuestion = formatQuestion(question);
+
+    setTimeout(() => {
+      questions = {
+        ...questions,
+        [formattedQuestion.id]: formattedQuestion
+      }
+      
+      users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          questions: users[authedUser].questions.concat([formattedQuestion.id])
+        }
+      }
+
+      res(formattedQuestion)
+    }, 1000)
+  })
+}
+
+export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      users = {
+        ...users,
+        [authedUser]: {
+          ...users[authedUser],
+          answers: {
+            ...users[authedUser].answers,
+            [qid]: answer
+          }
+        }
+      }
+
+      questions = {
+        ...questions,
+        [qid]: {
+          ...questions[qid],
+          [answer]: {
+            ...questions[qid][answer],
+            votes: questions[qid][answer].votes.concat([authedUser])
+          }
+        }
+      }
+
+      res()
+    }, 500)
+  })
+}
