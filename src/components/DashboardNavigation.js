dd27d755ac5732e2d56fd97470e8e31a8e2f5973
Omar <omarmuhammed1998.om@gmail.com>
2021-06-06 20:19:53 +0200

    styling changes

--- src/components/DashboardNavigation.js
+++ src/components/DashboardNavigation.js
@@ -4,19 +4,19 @@ export default class DashboardNavigation extends Component {
   render() {
     return (
       <div className="poll-nav">
-        <a href='#'>
+        <a href="#">
           <img
+            height="22px"
             alt="unanswered tab"
             src="https://www.freepnglogos.com/uploads/question-mark-png/question-mark-png-letter-profro-questions-glad-you-didn-7.png"
-            height="23px"
           />
           <p>Unanswered Questions</p>
         </a>
-        <a href='#'>
+        <a href="#">
           <img
+            height="22px"
             alt="answered tab"
             src="https://png.pngtree.com/element_our/20190601/ourmid/pngtree-right-answer-icon-image_1344787.jpg"
-            height="23px"
           />
           <p>Answered Questions</p>
         </a>
