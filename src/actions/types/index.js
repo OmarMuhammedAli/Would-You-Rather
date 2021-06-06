661615f6427ce2616e41360ce1eac04586591227
Omar <omarmuhammed1998.om@gmail.com>
2021-06-06 02:13:13 +0200

    created action types directory

---
+++ src/actions/types/index.js
@@ -0,0 +1,10 @@
+/**  Questions Types  */
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+
+/**  Users Types  */
+export const RECEIVE_USERS = 'RECEIVE_USERS'
+
+
+/**  Authed User Types  */
+export const SET_AUTHED_USER = 'SET_AUTHED_USER'
+
