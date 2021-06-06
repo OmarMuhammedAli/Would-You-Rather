fa1500746a22839576ce91208e2b7b566089c2b2
Omar <omarmuhammed1998.om@gmail.com>
2021-06-06 16:23:01 +0200

    enabled conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import {handleInitialFetch} from '../actions/shared'
-
+import { handleInitialFetch } from "../actions/shared";
+import LoginView from "./LoginView";
+import DashboardNavigation from "./DashboardNavigation";
+import LoadingBar from "react-redux-loading";
 
 class App extends Component {
-    componentDidMount() {
-        this.props.dispatch(handleInitialFetch())
-    }
-    render() {
-    const { questionsIDs } = this.props;
+  componentDidMount() {
+    this.props.dispatch(handleInitialFetch());
+  }
+  render() {
+    const { questionsIDs, loggedIn, loading } = this.props;
     return (
-      <div>
-        <ul>
-          {questionsIDs.map((id) => (
-            <li key={id}>{id}</li>
-          ))}
-        </ul>
-      </div>
+      <Fragment>
+        <LoadingBar />
+        <div className="app-content">
+          {loading ? null : !loggedIn ? <LoginView /> : <DashboardNavigation />}
+        </div>
+      </Fragment>
     );
   }
 }
 
-const mapStateToProps = ({ questions }) => ({
+const mapStateToProps = ({ questions, authedUser }) => ({
   questionsIDs: Object.keys(questions),
-  questions
+  loggedIn: authedUser !== "",
+  loading: authedUser === null,
 });
 
 export default connect(mapStateToProps)(App);
