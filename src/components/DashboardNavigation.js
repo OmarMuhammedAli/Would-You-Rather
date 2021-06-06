b38b49d59585088f59dfb74f162d18ccc82a482c
Omar <omarmuhammed1998.om@gmail.com>
2021-06-06 18:30:01 +0200

    updates on the nav component of the dashboard

--- src/components/DashboardNavigation.js
+++ src/components/DashboardNavigation.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class DashboardNavigation extends Component {
-    render() {
-        return (
-            <div className='poll-nav'>
-                <button>
-                    <img alt='unanswered tab' src='https://www.freepnglogos.com/uploads/question-mark-png/question-mark-png-letter-profro-questions-glad-you-didn-7.png' height='23px'/>
-                    <p>Unanswered Questions</p>
-                </button>
-                <button>
-                    <img alt='answered tab' src='https://png.pngtree.com/element_our/20190601/ourmid/pngtree-right-answer-icon-image_1344787.jpg' height='23px'/>
-                    <p>Answered Questions</p>
-                </button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="poll-nav">
+        <a href='#'>
+          <img
+            alt="unanswered tab"
+            src="https://www.freepnglogos.com/uploads/question-mark-png/question-mark-png-letter-profro-questions-glad-you-didn-7.png"
+            height="23px"
+          />
+          <p>Unanswered Questions</p>
+        </a>
+        <a href='#'>
+          <img
+            alt="answered tab"
+            src="https://png.pngtree.com/element_our/20190601/ourmid/pngtree-right-answer-icon-image_1344787.jpg"
+            height="23px"
+          />
+          <p>Answered Questions</p>
+        </a>
+      </div>
+    );
+  }
 }
