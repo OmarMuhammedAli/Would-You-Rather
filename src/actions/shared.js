63a0829a4019514a98a4bff2a3c5f2ec00bc6ab8
Omar <omarmuhammed1998.om@gmail.com>
2021-06-06 16:23:22 +0200

    added loading bar indicator

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,10 +2,13 @@ import {getInitialData} from '../utils/api'
 import {receiveQuestions} from './questions'
 import {receiveUsers} from './users'
 import {setAuthedUser} from './authedUser'
+import {showLoading, hideLoading} from 'react-redux-loading'
 
 export const handleInitialFetch = (authedUser='') => async (dispatch) => {
+    dispatch(showLoading())
     const {users, questions} = await getInitialData()
     dispatch(receiveUsers(users))
     dispatch(receiveQuestions(questions))
     dispatch(setAuthedUser(authedUser))
+    dispatch(hideLoading())
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux'
 import questions from './questions'
 import users from './users'
 import authedUser from './authedUser'
+import {loadingBarReducer} from 'react-redux-loading'
 
 export default combineReducers({
     questions,
     users,
-    authedUser
+    authedUser,
+    loadingBar: loadingBarReducer
 })
