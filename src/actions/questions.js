ad1cd460edb5669d9700169dfac504ac0015dcf0
Omar <omarmuhammed1998.om@gmail.com>
2021-06-07 22:50:45 +0200

    modifications on the action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,42 +1,48 @@
-import {RECEIVE_QUESTIONS, ADD_ANSWER, ADD_QUESTION} from './types'
-import {saveQuestionAnswer, saveQuestion} from '../utils/api'
-import {formatQuestion} from '../utils/helpers'
+import { RECEIVE_QUESTIONS, ADD_ANSWER, ADD_QUESTION } from "./types";
+import { saveQuestionAnswer, saveQuestion } from "../utils/api";
+import { formatQuestion } from "../utils/helpers";
 
-export const receiveQuestions = questions => ({
-    type: RECEIVE_QUESTIONS,
-    questions
-})
+export const receiveQuestions = (questions) => ({
+  type: RECEIVE_QUESTIONS,
+  questions,
+});
 
-const addAnswer = ({qid, authedUser, answer}) => ({
-    type: ADD_ANSWER,
-    authedUser,
-    qid,
-    answer
-})
+const addAnswer = ({ qid, authedUser, answer }) => ({
+  type: ADD_ANSWER,
+  authedUser,
+  qid,
+  answer,
+});
 
-const addQuestion = ({optionOneText, optionTwoText, authedUser}) => ({
-    type: ADD_QUESTION,
-    question: formatQuestion({optionOneText, optionTwoText, author: authedUser})
-})
+const addQuestion = ({ optionOneText, optionTwoText, authedUser }) => ({
+  type: ADD_QUESTION,
+  question: formatQuestion({
+    optionOneText,
+    optionTwoText,
+    author: authedUser,
+  }),
+});
 
 export const handleAddAnswer = (qid, answer) => async (dispatch, getState) => {
-    const {authedUser} = getState()
-    dispatch(addAnswer({qid, answer, authedUser}))
-    try {
-        return saveQuestionAnswer({ qid, answer, authedUser })
-    } catch (e) {
-        console.warn('Error Occured in handleAddAnswer: ', e)
-        alert('Something went wrong. Please try again!')
-    }
-}
+  const { authedUser } = getState();
+  dispatch(addAnswer({ qid, answer, authedUser }))
+  return saveQuestionAnswer({ authedUser, qid, answer })
+    .catch((e) => {
+      console.warn("Error Occured in handleAddAnswer: ", e);
+      alert("Something went wrong. Please try again!");
+    });
+};
 
-export const handleAddQuestion = (optionOneText, optionTwoText) => async (dispatch, getState) => {
-    const {authedUser} = getState()
+export const handleAddQuestion =
+  (optionOneText, optionTwoText) => async (dispatch, getState) => {
+    const { authedUser } = getState();
     try {
-        await saveQuestion({ optionOneText, optionTwoText, author: authedUser })
-        return dispatch(addQuestion({ optionOneText, optionTwoText, authedUser }))
+      await saveQuestion({ optionOneText, optionTwoText, author: authedUser });
+      return dispatch(
+        addQuestion({ optionOneText, optionTwoText, authedUser })
+      );
     } catch (e) {
-        console.warn('Error Occured in handleAddQuestion: ', e)
-        alert('Something went wrong. Please try again!')
+      console.warn("Error Occured in handleAddQuestion: ", e);
+      alert("Something went wrong. Please try again!");
     }
-} 
+  };
