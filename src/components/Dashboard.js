768977c181ca5be1c0270337fbf39d6c5449fdf9
Omar <omarmuhammed1998.om@gmail.com>
2021-06-07 03:03:42 +0200

    allowed navigation between answered and unanswered polls

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import DashboardNavigation from "./DashboardNavigation";
-import QuestionsCollection from './QuestionsCollection'
+import QuestionsCollection from "./QuestionsCollection";
 
 class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      status: "unAnswered",
+    };
+  }
+  handleStatusChange = (status) => {
+    this.setState({
+      status,
+    });
+  };
   render() {
     const { answeredIDs, unAnsweredIDs } = this.props;
-    
+    const { status } = this.state;
     return (
       <div className="poll">
-        <DashboardNavigation />
-        <QuestionsCollection ids={answeredIDs} status='answered'/>
-        <QuestionsCollection ids={unAnsweredIDs} status=''/>
+        <div className="poll-nav">
+          <button onClick={() => this.handleStatusChange("unAnswered")}>
+            <img
+              height="22px"
+              alt="unanswered tab"
+              src="https://www.freepnglogos.com/uploads/question-mark-png/question-mark-png-letter-profro-questions-glad-you-didn-7.png"
+            />
+            <p>Unanswered Questions</p>
+          </button>
+          <button onClick={() => this.handleStatusChange("answered")}>
+            <img
+              height="22px"
+              alt="answered tab"
+              src="https://png.pngtree.com/element_our/20190601/ourmid/pngtree-right-answer-icon-image_1344787.jpg"
+            />
+            <p>Answered Questions</p>
+          </button>
+        </div>
+        <QuestionsCollection
+          ids={status === "answered" ? answeredIDs : unAnsweredIDs}
+          status={status}
+        />
       </div>
     );
   }
@@ -19,16 +48,16 @@ class Dashboard extends Component {
 
 const mapStateToProps = ({ questions, authedUser, users }) => {
   const answeredQuestionsIDs = Object.keys(users[authedUser].answers);
-    
+
   const unAnsweredIDs = Object.values(questions)
-  .filter((questionID) => !answeredQuestionsIDs.includes(questionID.id))
-  .sort((a, b) => b.timestamp - a.timestamp)
-  .map(question => question.id);
+    .filter((questionID) => !answeredQuestionsIDs.includes(questionID.id))
+    .sort((a, b) => b.timestamp - a.timestamp)
+    .map((question) => question.id);
 
   const answeredIDs = Object.values(questions)
     .filter((questionID) => answeredQuestionsIDs.includes(questionID.id))
     .sort((a, b) => b.timestamp - a.timestamp)
-    .map(question => question.id);
+    .map((question) => question.id);
   return {
     answeredIDs,
     unAnsweredIDs,
