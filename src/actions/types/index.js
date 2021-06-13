2c21c8c608a7bd3cae3e4b8b8800daf8344bb662
Omar Ali <omarmuhammed1998.om@gmail.com>
2021-06-13 09:22:02 +0200

    Separate action types for better readability

--- src/actions/types/index.js
+++ src/actions/types/index.js
@@ -1,12 +1,12 @@
 /**  Questions Types  */
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
-export const ADD_ANSWER = 'ADD_ANSWER'
-export const ADD_QUESTION = 'ADD_QUESTION'
 
 /**  Users Types  */
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 
-
 /**  Authed User Types  */
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 
+/** Shared Types */
+export const ADD_ANSWER = 'ADD_ANSWER'
+export const ADD_QUESTION = 'ADD_QUESTION'
